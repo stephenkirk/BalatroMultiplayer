fb4364ed431d5def57b8fa9caead5446538cd134
Stephen Kirk <me@thestephen.net>
2025-08-15 14:37:07 +0200

    remove junk

--- compatibility/FantomsPreview.lua
+++ compatibility/FantomsPreview.lua
@@ -3,8 +3,4 @@ if not MP.INTEGRATIONS.FantomsPreview then
 	return
 end
 
--- Load FantomsPreview files in the correct order
--- MP.load_mp_dir("compatibility/Preview")
--- MP.load_mp_dir("compatibility/Preview/Jokers")
-
 sendInfoMessage("FantomsPreview integration loaded", "MULTIPLAYER")
