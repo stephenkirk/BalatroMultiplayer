b13534816e1fa63d9512db6a1edd32041988614e
Stephen Kirk <me@thestephen.net>
2026-02-18 23:20:38 +0100

    silent ban reworks

--- rulesets/traditional.lua
+++ rulesets/traditional.lua
@@ -2,7 +2,13 @@ MP.Ruleset({
 	key = "traditional",
 	multiplayer_content = true,
 	standard = true,
-	banned_silent = {},
+	banned_silent = {
+		"j_hanging_chad",
+		"j_idol",
+		"j_ticket",
+		"j_selzer",
+		"j_turtle_bean",
+	},
 	banned_jokers = {
 		"j_mp_speedrun",
 		"j_mp_conjoined_joker",
@@ -16,11 +22,11 @@ MP.Ruleset({
 	banned_tags = {},
 	banned_blinds = {},
 	reworked_jokers = {
-		"j_hanging_chad",
-		"j_idol",
-		"j_ticket",
-		"j_selzer",
-		"j_turtle_bean",
+		"j_mp_hanging_chad",
+		"j_mp_idol",
+		"j_mp_ticket",
+		"j_mp_seltzer",
+		"j_mp_turtle_bean",
 	},
 	reworked_consumables = {
 		"c_mp_ouija_standard",
